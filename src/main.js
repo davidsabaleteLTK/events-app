d5485811022fd4e310a6ed961de51c50ea7a4232
David Sabalete Rodríguez <david.sabalete@lovetoknow.com>
2019-12-18 10:54:59 +0100

    Adding authentication to a Vuejs app

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bulma/css/bulma.css";
 
-Vue.config.productionTip = false
+import { domain, clientId } from "../auth_config.json";
+
+import { Auth0Plugin } from "./auth";
+
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
